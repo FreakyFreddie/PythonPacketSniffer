1984d92c09d32ed742f3a68ada2b727e37a39aa7
dennisve <dennis_van_e@hotmail.com>
2016-04-12 09:38:24 +0200

    Unpacking ethernet headers
    also added protocol choice based on the EtherType

--- Capture-Packet.py
+++ Capture-Packet.py
@@ -36,3 +36,35 @@ def create_socket()
 def MAC_address(packet):
 	MAC = "%.2x:%.2x:%.2x:%.2x:%.2x:%.2x" % (hexcode(packet[0], packet[1], packet[2], packet[3], packet[4],  packet[5])
 	return MAC
+	
+#Extracting packets from socket
+def extract_packet(sock)
+	packetlog = open('packetlog.txt', 'a')
+	
+	#returns packet in hex from socket with bufsize 65565
+	#returns packet as string
+	packet = sock.recv(65565)
+	
+	#The length of the ethernet header is 14 bytes (layer 2 ethernet frame)
+	eth_length = 14
+	
+	#First 14 bytes are ethernet header
+	eth_header = packet[0:eth_length]
+	
+	#Unpack eth_header string according to the given format !6s6sH
+	#!indicates we don't know if the data is big or little endian
+	#s indicates a character (6xchar 6xchar)
+	#H indicates an unsigned short int (1xunsigned short int)
+	#Char is 1 byte, short int is 2 bytes
+	#MAC address format is 6 groups of 2 hexadecimal digits
+	eth = struct.unpack(!6s6sH, eth_header)
+	
+	#protocol used is short int from eth_header
+		#NOTE: Some systems use little endian order (like intel)
+		#we need to swap the bytes on those systems to get a uniform result
+		#ntohs switches network byte order to host byte order
+		#should any byte order problems occur, try implementing the ntohs function
+	eth_protocol = eth[2]
+	
+	#write MAC addresses to file
+	print 'Destination MAC : ' + eth_addr(packet[0:6]) + ' Source MAC : ' + eth_addr(packet[6:12]) + ' Protocol : ' + str(eth_protocol)
